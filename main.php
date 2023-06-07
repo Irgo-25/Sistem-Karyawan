e5a5cd9c7818d8344f7a0da5252efc2fa102e361
Irgo Satya <irgosg@gmail.com>
2023-06-07 23:59:50 +0700

    Update malam 070623

--- main.php
+++ main.php
@@ -108,6 +108,7 @@ session_start();
     <script src="assets/DataTables/Buttons-2.3.6/js/buttons.html5.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 
     <!-- FontAwesome -->
     <script src="assets/fontawesome6/js/all.min.js"></script>
