58aac4ff10b5930d29cb121c42302707189fa402
Irgo-25 <irgosg@gmail.com>
2023-06-14 16:02:39 +0700

    update beranda

--- modules/beranda/view.php
+++ modules/beranda/view.php
@@ -26,28 +26,13 @@
             <a href="?page=data-karyawan">
               <div class="inner">
                 <i class="fa fa-address-card fa-2xl"></i>
-                <h3>150</h3>
+                <h3></h3>
               </div>
               <div class="icon">
                 <i class="ion ion-bag"></i>
               </div>
               <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </a>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-8">
-          <!-- small box -->
-          <div class="small-box bg-success">
-            <div class="inner">
-              <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-              <p>Bounce Rate</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-stats-bars"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
